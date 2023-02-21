fd372a6a2298d024c1ea1b7a78b16734746cbac3
saad-rahid <saadalrahid13@gmail.com>
2023-02-21 15:05:03 +0600

    Complete signup season

--- main.js
+++ main.js
@@ -12,8 +12,24 @@ $(document).ready(function () {
       600: {
         items: 3,
       },
-      1000: {
-        items: 3,
+    },
+  });
+});
+
+
+$(document).ready(function () {
+  $(".testimonial__cards").owlCarousel({
+    loop: true,
+    margin: 5,
+    responsiveClass: true,
+    nav: true,
+    dots:false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
       },
     },
   });
